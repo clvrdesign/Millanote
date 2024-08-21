113de4a6a42e3d05ea7ca49b704a7b23e50f512a
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-21 19:36:39 +0200

    "Update footer copyright text from 'NoteApp' to 'Millanotes'"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="max-w-[1200px] mx-auto max-w-screen py-5">
       <div className="border-t border-t-slate-50 pt-5">
         <ul className="flex justify-between flex-col items-center md:flex-row text-slate-500"> 
-          <li><small>&copy; {copyright} NoteApp | All Rights Reserved</small></li>
+          <li><small>&copy; {copyright} Millanotes | All Rights Reserved</small></li>
           <li><small>Terms & Conditions</small></li>
         </ul>
       </div>
